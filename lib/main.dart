846a944bd1f8f21fb9eb397af15ca50778c64a7b
JorgeGonSan <162482388+JorgeGonSan@users.noreply.github.com>
2026-03-19 18:50:19 +0100

    UI basica del contador con botones +, - y reset inizializado a cero, sin funciones establecidas

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,57 @@ class MyContadorAvanzaado extends StatefulWidget {
 }
 
 class _MyContadorAvanzaadoState extends State<MyContadorAvanzaado> {
+  int contador = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Contador avanzado")),
-        body: Column(),
+        appBar: AppBar(
+          title: Text("Contador avanzado"),
+          backgroundColor: Colors.grey[300],
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(contador.toString(), style: TextStyle(fontSize: 50)),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(40),
+                      ),
+
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.add),
+                      ),
+                    ),
+                    SizedBox(width: 30),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(40),
+                      ),
+
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.remove),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 30),
+                ElevatedButton(onPressed: () {}, child: Text("Reset")),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
