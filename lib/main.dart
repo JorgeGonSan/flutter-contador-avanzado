510f6d9836562729444f767d19d461895704e5e3
JorgeGonSan <162482388+JorgeGonSan@users.noreply.github.com>
2026-04-07 18:38:02 +0200

    Separo configuración y wodget de la pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,104 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyContadorAvanzaado());
+  runApp(const MyApp());
 }
 
-class MyContadorAvanzaado extends StatefulWidget {
-  const MyContadorAvanzaado({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  State<MyContadorAvanzaado> createState() => _MyContadorAvanzaadoState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: MyContadorAvanzado());
+  }
 }
 
-class _MyContadorAvanzaadoState extends State<MyContadorAvanzaado> {
+class MyContadorAvanzado extends StatefulWidget {
+  const MyContadorAvanzado({super.key});
+
+  @override
+  State<MyContadorAvanzado> createState() => _MyContadorAvanzadoState();
+}
+
+class _MyContadorAvanzadoState extends State<MyContadorAvanzado> {
   int contador = 0;
   String mensaje = "";
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Contador avanzado"),
-          backgroundColor: Colors.grey[300],
-        ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(contador.toString(), style: TextStyle(fontSize: 50)),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(40),
-                      ),
-
-                      child: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            if (contador < 20) {
-                              contador++;
-                            }
-                            if (contador == 20) {
-                              mensaje = "No puede ser mayor que 20";
-                            } else {
-                              mensaje = "";
-                            }
-                          });
-                        },
-                        icon: Icon(Icons.add),
-                      ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contador avanzado"),
+        backgroundColor: Colors.grey[300],
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(contador.toString(), style: TextStyle(fontSize: 50)),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(40),
                     ),
-                    SizedBox(width: 30),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(40),
-                      ),
 
-                      child: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            if (contador > 0) {
-                              contador--;
-                            }
-                            if (contador == 0) {
-                              mensaje = "No puede ser menor que 0";
-                            } else {
-                              mensaje = "";
-                            }
-                          });
-                        },
-                        icon: Icon(Icons.remove),
-                      ),
+                    child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          if (contador < 20) {
+                            contador++;
+                          }
+                          if (contador == 20) {
+                            mensaje = "No puede ser mayor que 20";
+                          } else {
+                            mensaje = "";
+                          }
+                        });
+                      },
+                      icon: Icon(Icons.add),
                     ),
-                  ],
-                ),
-                SizedBox(height: 30),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      contador = 0;
-                    });
-                  },
-                  child: Text("Reset"),
-                ),
-                Text(mensaje),
-              ],
-            ),
+                  ),
+                  SizedBox(width: 30),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+
+                    child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          if (contador > 0) {
+                            contador--;
+                          }
+                          if (contador == 0) {
+                            mensaje = "No puede ser menor que 0";
+                          } else {
+                            mensaje = "";
+                          }
+                        });
+                      },
+                      icon: Icon(Icons.remove),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    contador = 0;
+                  });
+                },
+                child: Text("Reset"),
+              ),
+              Text(mensaje),
+            ],
           ),
         ),
       ),
